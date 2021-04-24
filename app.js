a2b225b8daa4a465467f5eedd0740822066ca233
arieltecay <arieltecay@gmail.com>
2021-04-24 18:55:38 -0300

    update & reformat of app.js

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-var express = require("express");
-var exphbs = require("express-handlebars");
-const mercadopago = require("mercadopago");
-const bodyParser = require("body-parser");
+const express = require("express");
+const exphbs = require("express-handlebars");
 
+const mercadopago = require("mercadopago");
 mercadopago.configure({
   access_token:
-    "APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398",
-  integrator_id: "dev_24c65fb163bf11ea96500242ac130004",
+    "APP_USR-8208253118659647-112521-dd670f3fd6aa9147df51117701a2082e-677408439",
+  integrator_id: "dev_2e4ad5dd362f11eb809d0242ac130004",
 });
 
-datos = {
-  urlApp: "https://arieltecay-mp-commerce-nodejs.herokuapp.com/",
-};
+const port = process.env.PORT || 3000;
 
-var app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+const app = express();
 
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
+
+app.use(express.static("assets"));
+app.use("/assets", express.static(__dirname + "/assets"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get("/", function (req, res) {
   res.render("home");
@@ -28,98 +28,81 @@ app.get("/detail", function (req, res) {
   res.render("detail", req.query);
 });
 
-app.get("/failure", function (req, res) {
-  res.render("status", req.query);
-});
-app.get("/pending", function (req, res) {
-  res.render("status", req.query);
-});
-app.get("/aproved", function (req, res) {
-  res.render("status", req.query);
-});
+app.post("/detail", (req, res) => {
+  const productImage = `${req.protocol}://${req.hostname}${req.body.productImage}`;
+  const productPrice = +req.body.productPrice;
 
-app.post("/notifications", function (req, res, next) {
-  console.log("**************************WEBHOOK**************************");
-  console.log(req.body);
-  console.log("************************FIN WEBHOOK************************");
-
-  let idPago = req.body.data.id;
-  if (idPago) {
-    console.log("--------------------------El ID de pago es: " + idPago);
-  }
-  res.sendStatus(201);
-});
-
-app.post("/iniciar_pago", function (req, res) {
-  let preference = {
+  const preference = {
     items: [
       {
         id: 1234,
-        title: req.body.title,
+        title: req.body.productDescription,
         description: "Dispositivo móvil de Tienda e-commerce",
-        picture_url: datos.urlApp + req.body.img,
         quantity: 1,
-        unit_price: Number(req.body.price),
-        external_reference: "arietlecay@gmail.com",
+        currency_id: "PEN",
+        picture_url: productImage,
+        unit_price: productPrice,
       },
     ],
-    external_reference: "arietlecay@gmail.com",
     payer: {
       name: "Lalo",
       surname: "Landa",
-      email: "test_user_63274575@testuser.com",
+      email: "test_user_46542185@testuser.com",
       phone: {
-        area_code: "11",
-        number: 22223333,
+        area_code: "52",
+        number: 5549737300,
+      },
+      identification: {
+        type: "DNI",
+        number: "22334445",
       },
       address: {
-        street_name: "False",
-        street_number: 123,
-        zip_code: "1111",
+        zip_code: "03940",
+        street_name: "Insurgentes Sur",
+        street_number: 1602,
       },
     },
-    back_urls: {
-      failure: `${datos.urlApp}/failure`,
-      pending: `${datos.urlApp}/pending`,
-      success: `${datos.urlApp}/aproved`,
-    },
-    auto_return: "approved",
-    notification_url: `${datos.urlApp}/notifications`,
     payment_methods: {
-      excluded_payment_methods: [
-        {
-          id: "amex",
-        },
-      ],
-      excluded_payment_types: [
-        {
-          id: "atm",
-        },
-      ],
+      excluded_payment_types: [{ id: "atm" }],
+      excluded_payment_methods: [{ id: "diners" }],
       installments: 6,
     },
+    back_urls: {
+      success: `${req.protocol}://${req.hostname}/responsePayment`,
+      pending: `${req.protocol}://${req.hostname}/responsePayment`,
+      failure: `${req.protocol}://${req.hostname}/responsePayment`,
+    },
+    notification_url: `${req.protocol}://${req.hostname}/notification`,
+    auto_return: "approved",
+    external_reference: "arieltecay@gmail.com",
   };
-
-  console.log("################### El preference ####################");
-  console.log(preference);
-  console.log("######################################################");
 
   mercadopago.preferences
     .create(preference)
-    .then(function (response) {
+    .then((response) => {
       console.log(response);
-      global.init_point = response.body.init_point;
-      console.log(global.init_point);
-      res.redirect(global.init_point);
+      res.redirect(response.body.init_point);
     })
-    .catch(function (error) {
-      console.log(error);
-    });
+    .catch((err) => console.log(err));
 });
 
-app.use(express.static("assets"));
+app.get("/responsePayment", (req, res) => {
+  if (req.query.collection_status == "approved") {
+    req.query.isSuccess = true;
+  }
 
-app.use("/assets", express.static(__dirname + "/assets"));
+  console.log("req.query");
+  console.log(req.query);
+
+  res.render("responsePayment", req.query);
+});
+
+app.post("/notification", (req, res) => {
+  console.log(" =========================================================== ");
+  console.log(req.body);
+  console.log(" =========================================================== ");
+  res.status(201);
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Servidor iniciado");
